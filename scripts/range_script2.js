d5a98db8e332418c5ea526d0ecd3e850d935db5a
Tibor Harcsa <tibor.harcsa@ericsson.com>
2017-06-26 01:07:05 +0200

    colour contrast function is separate now

--- scripts/range_script2.js
+++ scripts/range_script2.js
@@ -21,15 +21,5 @@ function changeCol(red,green,blue,targ) {
     console.log("the colour: " + theCol);
     document.getElementById(targ).innerHTML = theCol;
     document.getElementById(targ).style.backgroundColor = theCol;
-    document.getElementById(targ).style.color = getContrastYIQ(theCol);
-}
-
-// source: https://24ways.org/2010/calculating-color-contrast
-function getContrastYIQ(hexcolor){
-    var r = parseInt(hexcolor.substr(1,2),16);
-    var g = parseInt(hexcolor.substr(3,2),16);
-    var b = parseInt(hexcolor.substr(5,2),16);
-    console.log("basis for yiq analysis: " + r + "," + g + "," + b);
-    var yiq = ((r*299)+(g*587)+(b*114))/1000;
-    return (yiq >= 128) ? 'black' : 'white';
+    document.getElementById(targ).style.color = getContrastColour(theCol);
 }
