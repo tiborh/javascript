b29f405625a95d3c165359cebdf73cfc36ce1eed
Tibor Harcsa <silurust@gmail.com>
2017-08-27 01:27:50 +0200

    array in unordered list

---
+++ scripts/make_unordered_list.js
@@ -0,0 +1,19 @@
+//from https://stackoverflow.com/questions/11128700/create-a-ul-and-fill-it-based-on-a-passed-array
+function make_ul(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
